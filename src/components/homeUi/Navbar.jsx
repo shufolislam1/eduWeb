2f22073bf3d8ea680534fdd9eb642a62b7f3202e
shufol islam <shufolislam1255@gmail.com>
2023-01-26 23:21:28 +0600

    title + logo changed

--- src/components/homeUi/Navbar.jsx
+++ src/components/homeUi/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                             <li><a>Item 3</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl text-white">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl text-white">Edu Web</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 text-white">
