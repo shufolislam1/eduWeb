386470fa821b661dd07a3335e4dd4e5091e85727
shufol islam <shufolislam1255@gmail.com>
2023-01-26 14:06:31 +0600

    button done

--- src/components/homeUi/HomeBanner.jsx
+++ src/components/homeUi/HomeBanner.jsx
@@ -6,7 +6,13 @@ const HomeBanner = () => {
             <div>
                 <h1>dfgrffdsdfg</h1>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat tempore maxime aliquam veniam! Laboriosam sunt sequi distinctio obcaecati illo cumque voluptates odit, aspernatur unde perspiciatis debitis! Harum veniam hic, aspernatur reprehenderit sapiente, assumenda consequatur ex voluptas nemo minus eius ad!</p>
-                <button className='btn'>some</button>
+                <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                    <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-pink-500 group-hover:translate-x-0 ease">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                    </span>
+                    <span class="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Join Us Free</span>
+                    <span class="relative invisible">Join Us Free</span>
+                </a>
             </div>
             <div>
                 <img src="https://i.ibb.co/vJFTw4b/twinkle-online-education.gif" alt="education" srcset="" />
--- src/components/homeUi/Navbar.jsx
+++ src/components/homeUi/Navbar.jsx
@@ -43,14 +43,13 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end gap-5">
-                    <a href="">                <button className="btn glass rounded-full">Glass button</button></a>
-                    <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
-                        <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                        </span>
-                        <span class="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
-                        <span class="relative invisible">Button Text</span>
-                    </a>
+                <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                    <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                    </span>
+                    <span class="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Login</span>
+                    <span class="relative invisible">Login</span>
+                </a>
                 </div>
             </div>
         </div>
