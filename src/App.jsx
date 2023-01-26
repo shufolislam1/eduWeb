cfb73237454a3aab62dd6c5664a8c189e2016a35
shufol islam <shufolislam1255@gmail.com>
2023-01-26 11:43:23 +0600

    folder structure done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div >
       <div className="navbar bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
