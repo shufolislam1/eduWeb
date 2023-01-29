9dbd18c92d01e845f9d35263b74a67e71b620b1e
shufol islam <shufolislam1255@gmail.com>
2023-01-29 12:35:05 +0600

    firebase added with env variable and google btn in log reg done

--- src/components/LogReg/Login.jsx
+++ src/components/LogReg/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
                     <div className="text-center lg:text-left">
                         <img src="https://i.ibb.co/QvSc8GN/login.gif" alt="login" srcSet="" />
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-opacity-50">
                         <div className="card-body ">
                             <div className="form-control">
                                 <label className="label">
@@ -29,7 +29,7 @@ const Login = () => {
                                     <a href="#" className="label-text-alt link link-hover text-pink-500">Forgot password?</a>
                                 </label>
                                 <label className="label mt-4 ">
-                                    <h1>Don't have an account?</h1>
+                                    <h1>New to Edu Web?</h1>
                                     <Link to='/registration' className="label-text-alt link link-hover text-pink-500">Join Now!</Link>
                                 </label>
                             </div>
@@ -42,6 +42,16 @@ const Login = () => {
                                     <span className="relative invisible">Login</span>
                                 </button>
                             </div>
+                        </div>
+                        <div className="divider">OR</div>
+                        <div className="h-20">
+                            <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                </span>
+                                <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Continue With Google</span>
+                                <span className="relative invisible">Continue With Google</span>
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/components/LogReg/Registration.jsx
+++ src/components/LogReg/Registration.jsx
@@ -13,8 +13,14 @@ const Registration = () => {
                         <div className="text-center lg:text-left">
                             <img className='ml-10' src="https://i.ibb.co/NYpGbmz/reg.gif" alt="registration" srcSet="" />
                         </div>
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-opacity-50">
                             <div className="card-body ">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" placeholder="name" className="input input-bordered" />
+                                </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Email</span>
@@ -43,6 +49,16 @@ const Registration = () => {
                                         <span className="relative invisible">Registration</span>
                                     </button>
                                 </div>
+                            </div>
+                            <div className="divider">OR</div>
+                            <div className="h-20">
+                                <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                                    <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                    </span>
+                                    <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Continue With Google</span>
+                                    <span className="relative invisible">Continue With Google</span>
+                                </button>
                             </div>
                         </div>
                     </div>
