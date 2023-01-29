6a970f9afa7684a0d4201dc4bf532e911ebf197a
shufol islam <shufolislam1255@gmail.com>
2023-01-29 18:55:55 +0600

    code restructured

--- src/components/LogReg/Login.jsx
+++ src/components/LogReg/Login.jsx
@@ -2,25 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../homeUi/Footer';
 import Navbar from '../homeUi/Navbar';
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import auth from '../../../firebase/firebase.config';
 
-
-// google login code
-
-const provider =new GoogleAuthProvider();
 
 const Login = () => {
-    const handleGoogleLogin = () => {
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch((error) => {
-            console.log('error: ', error);
-        })
-    }
     return (
         <div className=' bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400'>
             <Navbar></Navbar>
@@ -62,7 +46,7 @@ const Login = () => {
                         </div>
                         <div className="divider">OR</div>
                         <div className="h-20">
-                            <button  onClick={handleGoogleLogin} className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                            <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
                                 <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
                                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                                 </span>
