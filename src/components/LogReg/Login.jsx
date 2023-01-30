df30e22023c9d6d20ac1168020528d400bd6da13
shufol islam <shufolislam1255@gmail.com>
2023-01-30 23:17:11 +0600

    react reveal added in log reg

--- src/components/LogReg/Login.jsx
+++ src/components/LogReg/Login.jsx
@@ -7,6 +7,8 @@ import Footer from '../homeUi/Footer';
 import Navbar from '../homeUi/Navbar';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import Jump from 'react-reveal/Jump';
+import Fade from 'react-reveal/Fade';
 
 // sweet alert things
 const MySwal = withReactContent(Swal);
@@ -71,55 +73,59 @@ const Login = () => {
     return (
         <div className=' bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400'>
             <Navbar></Navbar>
-            <div className="hero min-h-screen  mx-auto">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <img src="https://i.ibb.co/QvSc8GN/login.gif" alt="login" srcSet="" />
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-opacity-50">
-                        <form onSubmit={handleLogin} className="card-body ">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+            <Jump>
+                <Fade>
+                    <div className="hero min-h-screen  mx-auto">
+                        <div className="hero-content flex-col lg:flex-row-reverse">
+                            <div className="text-center lg:text-left">
+                                <img src="https://i.ibb.co/QvSc8GN/login.gif" alt="login" srcSet="" />
                             </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
-                                <label className="label mt-4 ">
-                                    <a href="#" className="label-text-alt link link-hover text-pink-500">Forgot password?</a>
-                                </label>
-                                <label className="label mt-4 ">
-                                    <h1>New to Edu Web?</h1>
-                                    <Link to='/registration' className="label-text-alt link link-hover text-pink-500">Join Now!</Link>
-                                </label>
+                            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-opacity-50">
+                                <form onSubmit={handleLogin} className="card-body ">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Email</span>
+                                        </label>
+                                        <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Password</span>
+                                        </label>
+                                        <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                        <label className="label mt-4 ">
+                                            <a href="#" className="label-text-alt link link-hover text-pink-500">Forgot password?</a>
+                                        </label>
+                                        <label className="label mt-4 ">
+                                            <h1>New to Edu Web?</h1>
+                                            <Link to='/registration' className="label-text-alt link link-hover text-pink-500">Join Now!</Link>
+                                        </label>
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                                            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                            </span>
+                                            <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Login</span>
+                                            <span className="relative invisible">Login</span>
+                                        </button>
+                                    </div>
+                                </form>
+                                <div className="divider">OR</div>
+                                <div className="h-20">
+                                    <button onClick={googleLoginHandler} className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                                        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                        </span>
+                                        <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Continue With Google</span>
+                                        <span className="relative invisible">Continue With Google</span>
+                                    </button>
+                                </div>
                             </div>
-                            <div className="form-control mt-6">
-                                <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
-                                    <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                    </span>
-                                    <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Login</span>
-                                    <span className="relative invisible">Login</span>
-                                </button>
-                            </div>
-                        </form>
-                        <div className="divider">OR</div>
-                        <div className="h-20">
-                            <button onClick={googleLoginHandler} className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
-                                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                </span>
-                                <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Continue With Google</span>
-                                <span className="relative invisible">Continue With Google</span>
-                            </button>
                         </div>
                     </div>
-                </div>
-            </div>
+                </Fade>
+            </Jump>
             <Footer></Footer>
         </div>
     );
--- src/components/LogReg/Registration.jsx
+++ src/components/LogReg/Registration.jsx
@@ -6,6 +6,8 @@ import Footer from '../homeUi/Footer';
 import Navbar from '../homeUi/Navbar';
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import Jump from 'react-reveal/Jump';
+import Fade from 'react-reveal/Fade';
 
 // sweet alert things
 const MySwal = withReactContent(Swal);
@@ -72,58 +74,62 @@ const Registration = () => {
         <div>
             <div className=' bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400'>
                 <Navbar></Navbar>
-                <div className="hero min-h-screen  mx-auto">
-                    <div className="hero-content flex-col lg:flex-row-reverse">
-                        <div className="text-center lg:text-left">
-                            <img className='ml-10' src="https://i.ibb.co/NYpGbmz/reg.gif" alt="registration" srcSet="" />
-                        </div>
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-opacity-50">
-                            <form onSubmit={handleRegistration} className="card-body ">
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Name</span>
-                                    </label>
-                                    <input name='name' type="text" placeholder="name" className="input input-bordered" required />
+                <Jump>
+                    <Fade>
+                        <div className="hero min-h-screen  mx-auto">
+                            <div className="hero-content flex-col lg:flex-row-reverse">
+                                <div className="text-center lg:text-left">
+                                    <img className='ml-10' src="https://i.ibb.co/NYpGbmz/reg.gif" alt="registration" srcSet="" />
                                 </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Email</span>
-                                    </label>
-                                    <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+                                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-opacity-50">
+                                    <form onSubmit={handleRegistration} className="card-body ">
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Name</span>
+                                            </label>
+                                            <input name='name' type="text" placeholder="name" className="input input-bordered" required />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Email</span>
+                                            </label>
+                                            <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Password</span>
+                                            </label>
+                                            <input name='password' type="password" placeholder="password" className="input input-bordered" required />
+                                            <label className="label mt-4 ">
+                                                <h1>Already have an account?</h1>
+                                                <Link to='/login' className="label-text-alt link link-hover text-pink-500">Login</Link>
+                                            </label>
+                                        </div>
+                                        <div className="form-control mt-6">
+                                            <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                                                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                                </span>
+                                                <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Registration</span>
+                                                <span className="relative invisible">Registration</span>
+                                            </button>
+                                        </div>
+                                    </form>
+                                    <div className="divider">OR</div>
+                                    <div className="h-20">
+                                        <button onClick={googleResgistrationHandler} className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
+                                            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                                            </span>
+                                            <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Continue With Google</span>
+                                            <span className="relative invisible">Continue With Google</span>
+                                        </button>
+                                    </div>
                                 </div>
-                                <div className="form-control">
-                                    <label className="label">
-                                        <span className="label-text">Password</span>
-                                    </label>
-                                    <input name='password' type="password" placeholder="password" className="input input-bordered" required />
-                                    <label className="label mt-4 ">
-                                        <h1>Already have an account?</h1>
-                                        <Link to='/login' className="label-text-alt link link-hover text-pink-500">Login</Link>
-                                    </label>
-                                </div>
-                                <div className="form-control mt-6">
-                                    <button className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
-                                        <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                        </span>
-                                        <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Registration</span>
-                                        <span className="relative invisible">Registration</span>
-                                    </button>
-                                </div>
-                            </form>
-                            <div className="divider">OR</div>
-                            <div className="h-20">
-                                <button onClick={googleResgistrationHandler} className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2  rounded-full shadow-md group">
-                                    <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-                                    </span>
-                                    <span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Continue With Google</span>
-                                    <span className="relative invisible">Continue With Google</span>
-                                </button>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </Fade>
+                </Jump>
                 <Footer></Footer>
             </div>
         </div>
